69b67b6c441dae921c65bfb2833b8bb17af2e123
Walisson Aguirra <walissonaguirra@icloud.com>
2024-05-13 19:50:25 -0300

    Adicionando arquivo `.editorconfig` e aplicando formatação no código

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-/** 
+/**
  * Nullish Coalescing Operator
- * 
- * O operador de coalescência nula (??) é um operador lógico que retorna 
- * o seu operando do lado direito quando o seu operador do lado esquerdo é 
- * null ou undefined. Caso contrário, ele retorna o seu operando do lado 
+ *
+ * O operador de coalescência nula (??) é um operador lógico que retorna
+ * o seu operando do lado direito quando o seu operador do lado esquerdo é
+ * null ou undefined. Caso contrário, ele retorna o seu operando do lado
  * esquerdo.
- * 
- * Ao contrário do operador lógico OR (||), o operando esquerdo é retornado 
+ *
+ * Ao contrário do operador lógico OR (||), o operando esquerdo é retornado
  * se houver um valor "falsy" => 0, '', [], false, undefined, null
- * 
+ *
  */
 
 let idade
 
 idade = 24
-console.log('Sua idade é ' + (idade || 'Não informado') + ' anos') 
+console.log('Sua idade é ' + (idade || 'Não informado') + ' anos')
 // R= Sua idade é 24 anos
 
 idade = 0 // 0, '', [], false, undefined, null
@@ -28,11 +28,11 @@ console.log('Sua idade é ' + (idade ?? 'Não informado'))
 
 /**
  * Object
- * 
- * O conceito de objetos em JavaScript pode ser entendido com objetos 
- * tangíveis da vida real. São basicamente estrutura de dados que 
+ *
+ * O conceito de objetos em JavaScript pode ser entendido com objetos
+ * tangíveis da vida real. São basicamente estrutura de dados que
  * podem armasena conjuntos de chave e valor.
- * 
+ *
  */
 
 let usuario = {
@@ -58,9 +58,9 @@ console.log(Object.keys(usuario)) // ['nome', 'idade', 'endereco']
 // Object.values() => retorna um vetor com todos os valores do objeto
 console.log(Object.values(usuario)) // ['Walisson Aguirra', 24, {...}]
 
-/** 
+/**
  * Object.entries() => retorna um vetor de vetores
- * 
+ *
  * [
  *  [
  *    "nome", // O primeiro indice contem o equivalente a chave
@@ -71,11 +71,11 @@ console.log(Object.values(usuario)) // ['Walisson Aguirra', 24, {...}]
  */
 console.log(Object.entries(usuario)) // [["nome", "Walisson Aguirra"], ...]
 
-/** 
+/**
  * Destructuring Assignment
- * 
+ *
  * Possibilita extrair dados de arrays ou objetos em variáveis.
- * 
+ *
  */
 
 let _nome = usuario.nome // nome == 'Walisson Aguirra'
@@ -84,13 +84,13 @@ console.log(_nome, _idade)
 
 // Usando desestruturação ficaria:
 
-let { __nome, __idade } = usuario 
+let { __nome, __idade } = usuario
 console.log(__nome, __idade) // nome == 'Walisson Aguirra', idade == 24
 
 let { nome: name, idade: age } = usuario
 console.log(name, age) // name == 'Walisson Aguirra', age == 24
 
-let { nome: __name, idade: _age, nickname = 'aguirra24' } = usuario 
+let { nome: __name, idade: _age, nickname = 'aguirra24' } = usuario
 console.log(nickname) // nickname === 'aguirra24'
 
 
@@ -173,7 +173,7 @@ let novoArray = _array.map(item => {
 })
 console.log(novoArray) // [1, 20, 3, 40, 5]
 
-let _novoArray = _array.filter(item => item % 2 === 0) 
+let _novoArray = _array.filter(item => item % 2 === 0)
 console.log(_novoArray) // 2, 4
 
 let __novoArray = _array
@@ -206,3 +206,4 @@ console.log(soma) // 15
 let _name = null
 let message = `Bem-vindo, ${name ? name : 'visitante'}`
 console.log(message) // Bem-vindo, visitante
+
